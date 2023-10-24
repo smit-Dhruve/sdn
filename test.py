d206ed597f3e10b2b94c9aba53882a7e7991f534
Sathyaprakash Sahoo <62632966+Encryptor-Sec@users.noreply.github.com>
2023-10-24 22:21:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 from mininet.net import Mininet
-from mininet.topo import SingleSwitchTopo, LinearTopo, TreeTopo
+from mininet.topo import Topo
 from mininet.cli import CLI
+
+class CustomTreeTopo(Topo):
+    def build(self, depth=2, fanout=2):
+        # Create the root switch
+        root_switch = self.addSwitch('s1')
+
+        # Recursive function to create tree-like structure
+        def add_tree_links(node, current_depth):
+            if current_depth == depth:
+                return
+            for i in range(fanout):
+                child_switch = self.addSwitch('s%s' % (len(self.switches) + 1))
+                self.addLink(node, child_switch)
+                add_tree_links(child_switch, current_depth + 1)
+
+        # Start building the tree
+        add_tree_links(root_switch, 0)
 
 def create_and_run_topology(topo, controller=None):
     net = Mininet(topo=topo, controller=controller)
@@ -16,6 +33,7 @@ create_and_run_topology(simple_topo)
 linear_topo = LinearTopo(3)
 create_and_run_topology(linear_topo)
 
-# Tree Topology
-tree_topo = TreeTopo(depth=2, fanout=2)
-create_and_run_topology(tree_topo)
+# Custom Tree Topology
+custom_tree_topo = CustomTreeTopo(depth=2, fanout=2)
+create_and_run_topology(custom_tree_topo)
+
